f9864de21972b69b394505a2c273b9a4eed0116c
arnas13 <ses.arnas@gmail.com>
2021-02-15 09:57:33 +0200

    tested Application constructor

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php 
+require_once 'core/Application.php';
 
-print "Almost Lara";
+use app\core\Application;
+
+$app = new Application();
+
+// $router = new Router();
+
+// $router->get('/', function() {
+//     return "this is home page";
+// });
+
+// $router->get('/about', function() {
+//     return "this is about page";
+// });
+
+//$app->run();
+
+
+
+
+
