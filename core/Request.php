122220f3bacbeeeea09fe1f8a00e76ff3859dcd4
arnas13 <ses.arnas@gmail.com>
2021-02-17 09:01:24 +0200

    added proper phpDoc

--- core/Request.php
+++ core/Request.php
@@ -2,19 +2,20 @@
 
 namespace app\core;
 
-/**
- * 
- * Get user page form uri
- * 
- * [REQUEST_URI] => /todos?id=5
- * extract /todos
- * 
- * Class Request 
- * @package app\core
- */
+
 class Request {
-    public function getPath() {
-        $path = $_SERVER['REQUEST_URI'] ?? '/AlmostLara/';
+    /**
+     * 
+     * Get user page form url
+     * 
+     * [REQUEST_URI] => /todos?id=5
+     * extract /todos
+     *   
+     * @return false/mixed/string
+     */
+
+    public function getPath() : string {
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
         $questionPosition = strpos($path, '?');
 
         if($questionPosition !== false) : 
