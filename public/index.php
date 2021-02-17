5ac0c326f7880f5876ee99546587814164a3d126
arnas13 <ses.arnas@gmail.com>
2021-02-17 10:53:22 +0200

    made mvc structure and moved index.php file to public. and Serving it from there using htaccess magic

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php 
+
+//phpinfo();exit;
+// require_once 'core/Application.php';
+// require_once 'core/Router.php';
+
+require_once '../vendor/autoload.php';
+
+use app\core\Application;
+
+$app = new Application();
+
+
+$app->router->get('/', function() {
+    return "this is home page";
+});
+
+$app->router->get('/about', function() {
+    return "this is about page";
+});
+
+$app->run();
+
+
+// $router->get('/about', function() {
+//     return "this is about page";
+// });
+
+//$app->run();
+
+
+
+
+
