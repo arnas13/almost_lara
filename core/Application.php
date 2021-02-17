94c7019235b3ccb1ab021d852e7756243cab232d
arnas13 <ses.arnas@gmail.com>
2021-02-17 11:56:38 +0200

    Application gets root dir as argument. And saves it as local static property

--- core/Application.php
+++ core/Application.php
@@ -17,12 +17,15 @@ class Application {
      * 
      * @var Router
      */
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
 
 
-    public function __construct()
+    public function __construct($rootPath)
     {
+        // static property assignment
+        self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->router = new Router($this->request);
         
--- public/index.php
+++ public/index.php
@@ -8,12 +8,15 @@ require_once '../vendor/autoload.php';
 
 use app\core\Application;
 
-$app = new Application();
+// print "<pre>";
+// var_dump(dirname(__DIR__));
+// print "</pre>";
+// exit();
+
+$app = new Application(dirname(__DIR__));
 
 
-$app->router->get('/', function() {
-    return "this is home page";
-});
+$app->router->get('/', 'home');
 
 $app->router->get('/about', 'about');
 
