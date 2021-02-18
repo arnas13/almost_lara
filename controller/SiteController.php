9035c83184f1015ede26c82f739ef048e25ff3c6
arnas13 <ses.arnas@gmail.com>
2021-02-18 10:39:32 +0200

    created SiteController.php and handleContact simple method

---
+++ controller/SiteController.php
@@ -0,0 +1,9 @@
+<?php 
+
+namespace app\controller;
+
+class SiteController {
+    public static function handleContact() {
+        return "handling form from site controller handle form method";
+    }
+}
