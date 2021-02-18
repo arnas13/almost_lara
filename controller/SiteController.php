44a779b58fc2e37f17977aea01bbb3a5a6b8b2df
arnas13 <ses.arnas@gmail.com>
2021-02-18 10:54:32 +0200

    contact method to handle get request

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -2,8 +2,26 @@
 
 namespace app\controller;
 
+use app\core\Application;
+
 class SiteController {
+    /**
+     * This serves the contact form view
+     *
+     * @return string
+     */
+    public static function contact() {
+        // lets render view
+        return Application::$app->router->renderView('contact');
+    }
+
+    /**
+     * This is were we handle post contact form.
+     *
+     * @return string
+     */
     public static function handleContact() {
         return "handling form from site controller handle form method";
     }
+    
 }
