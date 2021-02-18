b28e11f09c50455eb0d01e0a6fb867dab6b4f2fe
arnas13 <ses.arnas@gmail.com>
2021-02-18 11:18:02 +0200

    method for home, passing params to renderView

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -6,6 +6,19 @@ use app\core\Application;
 
 class SiteController {
     /**
+     * This handles Home page get request
+     *
+     * @return string
+     */
+    public static function home(){
+        $params = [
+            'name' => "AlmostLara",
+            'subtitle' => "This is a nice way to learn PHP"
+        ];
+
+        return Application::$app->router->renderView('home', $params);
+    }
+    /**
      * This serves the contact form view
      *
      * @return string
