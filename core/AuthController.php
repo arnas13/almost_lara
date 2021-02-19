70bfd468da99bcb023335e7a6852e8631fca8cfe
arnas13 <ses.arnas@gmail.com>
2021-02-19 11:18:11 +0200

    new Controller handling register method post and get

---
+++ core/AuthController.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace app\core;
+
+/**
+ * Responsible for handling login and registe
+ * 
+ * Class AuthController
+ * @package app\core
+ */
+class AuthController extends Controller{
+    public function login() {
+        return $this->render('login');
+    }
+
+    public function register(Request $request) {
+        if($request->isGet()) :
+            return $this->render('register');
+        endif;
+        
+        if($request->isPost()) :
+            return "Validating form";
+        endif;
+    }
+}
