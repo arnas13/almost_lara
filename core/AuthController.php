5f50d4787af04c0e393f6e5c20aa256d10bedfb8
arnas13 <ses.arnas@gmail.com>
2021-02-19 12:10:40 +0200

    setLayout ('auth;) function call

--- core/AuthController.php
+++ core/AuthController.php
@@ -10,11 +10,14 @@ namespace app\core;
  */
 class AuthController extends Controller{
     public function login() {
+        // have ability to change layout
+        $this->setLayout('auth');
         return $this->render('login');
     }
 
     public function register(Request $request) {
         if($request->isGet()) :
+            $this->setLayout('auth');
             return $this->render('register');
         endif;
         
