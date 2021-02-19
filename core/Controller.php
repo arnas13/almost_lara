2afad91ea2642ee96a51b2307ae8e72cf85fe885
arnas13 <ses.arnas@gmail.com>
2021-02-19 09:01:27 +0200

    base Controller with render method

---
+++ core/Controller.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace app\core;
+
+/**
+ * Class Controller.
+ * 
+ * our base controller class
+ * 
+ * @package app\core
+ */
+class Controller {
+    /**
+     * We render the base view with params
+     *
+     * @param string $view
+     * @param array $params
+     * @return string / string[]
+     */
+    public function render($view, $params = []) {
+        return Application::$app->router->renderView($view, $params);
+    }
+}
