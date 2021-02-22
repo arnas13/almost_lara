ffd889ba3db7c3107a97971e863ecc9c330404c3
arnas13 <ses.arnas@gmail.com>
2021-02-22 09:38:28 +0200

    Added validation and used to validate form

--- core/AuthController.php
+++ core/AuthController.php
@@ -9,6 +9,13 @@ namespace app\core;
  * @package app\core
  */
 class AuthController extends Controller{
+    public Validation $vld;
+
+    public function __construct()
+    {
+        $this->vld = new Validation;
+    }
+
     public function login() {
         // have ability to change layout
         $this->setLayout('auth');
@@ -40,7 +47,16 @@ class AuthController extends Controller{
 
             // request is post and we need to pull user data
             $data = $request->getBody();
-            
+
+            $data['errors']['nameErr'] = $this->vld->validateName($data['name']);
+
+            $data['errors']['emailErr'] = $this->vld->validateEmail($data['email']);
+            // $data['errors']['emailErr'] = $this->vld->validateEmail($data['email'], $this->userModel);
+
+            $data['errors']['passwordErr'] = $this->vld->validatePassword($data['password'], 6, 10);
+
+            $data['errors']['confirmPasswordErr'] = $this->vld->confirmPassword($data['confirmPassword']);
+
             // echo "<pre>";
             // var_dump($data);
             // echo "</pre>";
