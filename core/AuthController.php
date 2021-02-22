9c38a719e5a9de2490c89bda5e0d408e0934b00a
arnas13 <ses.arnas@gmail.com>
2021-02-22 15:04:13 +0200

    add userModel.php to AuthController.php and use it to register

--- core/AuthController.php
+++ core/AuthController.php
@@ -1,84 +1,93 @@
-<?php 
+<?php
+
 
 namespace app\core;
 
+use app\model\UserModel;
+
 /**
- * Responsible for handling login and registe
- * 
+ * Respossible for handling login and register.
+ *
  * Class AuthController
  * @package app\core
  */
-class AuthController extends Controller{
+class AuthController extends Controller
+{
     public Validation $vld;
+    protected UserModel $userModel;
 
     public function __construct()
     {
-        $this->vld = new Validation;
+        $this->vld = new Validation();
+        $this->userModel = new UserModel();
     }
 
-    public function login() {
-        // have ability to change layout
+    public function login()
+    {
+        // have ability to change laout
         $this->setLayout('auth');
         return $this->render('login');
     }
 
-    public function register(Request $request) {
-        if($request->isGet()) :
-            //$this->setLayout('auth');
+    public function register(Request $request)
+    {
+        if ($request->isGet()) :
+//            $this->setLayout('auth');
 
-        // create data
-        $data = [
-            'name' => '',            
-            'email' => '',            
-            'password' => '',            
-            'confirmPassword' => '',            
-            'errors' => [            
-                'nameErr' => '',            
-                'emailErr' => '',            
-                'passwordErr' => '',            
-                'confirmPasswordErr' => '',            
-                ],            
-            'currentPage' => 'register',            
+            // create data
+            $data = [
+                'name'      => '',
+                'email'     => '',
+                'password'  => '',
+                'confirmPassword' => '',
+                'errors' => [
+                    'nameErr'      => '',
+                    'emailErr'     => '',
+                    'passwordErr'  => '',
+                    'confirmPasswordErr' => '',
+                ],
+                'currentPage' => 'register',
             ];
+
+
+
             return $this->render('register', $data);
         endif;
-        
-        if($request->isPost()) :
 
-            // request is post and we need to pull user data
+        if ($request->isPost()) :
+
+            // reques is post and we need to pull user data
             $data = $request->getBody();
 
             $data['errors']['nameErr'] = $this->vld->validateName($data['name']);
 
-            $data['errors']['emailErr'] = $this->vld->validateEmail($data['email']);
-            // $data['errors']['emailErr'] = $this->vld->validateEmail($data['email'], $this->userModel);
+            $data['errors']['emailErr'] = $this->vld->validateEmail($data['email'], $this->userModel);
 
             $data['errors']['passwordErr'] = $this->vld->validatePassword($data['password'], 6, 10);
 
             $data['errors']['confirmPasswordErr'] = $this->vld->confirmPassword($data['confirmPassword']);
 
-            // if there are no errror
-            if($this->vld->ifEmptyArr($data['errors'])) :
+            // if there are no errors
+            if ($this->vld->ifEmptyArr($data['errors'])) :
 
-                // hash password // safe way to store pw
+
+                // hash password // save way to store pass
                 $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
 
-                // create user 
-                if($this->userModel->register($data)) {
+                // create user
+                if ($this->userModel->register($data)) {
                     // success user added
                     // set flash msg
-                    flash('register_success', "You have registered successfully");
-                    // header("Loacation: " . URLROOT . "/users/login");
-                    redirect('/login');
+//                    flash('register_success', 'You have registered successfully');
+                    // header("Location: " . URLROOT . "/users/login");
+//                    redirect('/login');
                 } else {
-                    die('something went wrong in adding user ');
+                    die('Something went wrong in adding user to db');
                 }
+
             endif;
 
-            // echo "<pre>";
-            // var_dump($data);
-            // echo "</pre>";
-            // exit;
+
 
 
             return $this->render('register', $data);
