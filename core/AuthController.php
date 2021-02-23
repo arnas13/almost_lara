d419b71cc5cf092b7ee3b9996c65363962eb0b06
arnas13 <ses.arnas@gmail.com>
2021-02-23 09:29:35 +0200

    CreateUserSession and logout methods

--- core/AuthController.php
+++ core/AuthController.php
@@ -54,14 +54,15 @@ class AuthController extends Controller
                 // email was found and password was entered
                 $loggedInUser = $this->userModel->login($data['email'], $data['password']);
 
-                var_dump($loggedInUser);
-                exit;
+                // var_dump($loggedInUser);
+                // exit;
 
                 if ($loggedInUser) {
                     // create session 
                     // password match
                     // die('email and passs match start session immediately');
-                   // $this->createUserSession($loggedInUser);
+                    $this->createUserSession($loggedInUser);
+                    $request->redirect('/posts');
                 } else {
                     $data['errors']['passwordErr'] = 'Wrong password or email';
                     // load view with errors
@@ -136,5 +137,29 @@ class AuthController extends Controller
 
             return $this->render('register', $data);
         endif;
+    }
+
+    /**
+     * if we have user we save its data in session
+     *
+     * @param  $userRow
+     * @return void
+     */
+    public function createUserSession($userRow)
+    {
+        $_SESSION['user_id'] = $userRow->id;
+        $_SESSION['user_email'] = $userRow->email;
+        $_SESSION['user_name'] = $userRow->name;
+       
+
+    public function logout()
+    {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+
+        session_destroy();
+
+        redirect('/users/login');
     }
 }
