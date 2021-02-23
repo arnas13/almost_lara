2dd0723d923f7d1ab64ff71a415c34ebbe53c2d4
arnas13 <ses.arnas@gmail.com>
2021-02-23 08:15:18 +0200

    fixed Router

--- core/Router.php
+++ core/Router.php
@@ -1,102 +1,102 @@
-<?php 
+<?php
+
 
 namespace app\core;
 
 /**
- * Class Router 
- * 
+ * Class Router
+ *
  * This is where we call controllers and methods
- * 
+ *
  * @package app\core
  */
-class Router {
+class Router
+{
     /**
-     * This will hold all routes
-     * 
+     * This will hold all routes.
+     *
      * routes [
-     * ['get' => [
-        * ['/' => function return,],
-        * ['/about' => 'about' ,],
-     * ],  
-     * ['post' => [
-        * ['/' => function return,],
-        * ['/contact' => function return,],
+     * ['get'  => [
+     *  ['/' => function return,],
+     *  ['/about' => 'about',],
      * ],
-     * 
+     * ['post' => [
+     *  ['/' => function return,],
+     *  ['/contact' => function return,],
+     * ]]
+     * ]
+     *
+     *
      * ];
-     * 
+     *
      * @var array
      */
     protected array $routes = [];
     public Request $request;
     public Response $response;
 
-    public function __construct($request, $response)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
         $this->response = $response;
     }
 
     /**
-     * Add get route and callback to routes array
+     * Add get route and callback fn to routes array
      *
      * @param string $path
-     * @param [type] $callback
+     * @param $callback
      */
-    public function get($path, $callback) {
-        $this->routes['get'][$path] =  $callback;
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
     }
 
     /**
-     * This creates post path and handling in route array.
+     * This creates post path and handling in routes array.
      *
-     * @param [type] $path
-     * @param [type] $callback
-     * @return void
+     * @param $path
+     * @param $callback
      */
-    public function post($path, $callback) {
-        $this->routes['post'][$path] =  $callback;
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
     }
 
     /**
      * Executes user function if it is set in routes array
-     *
      */
-    public function resolve() {
+    public function resolve()
+    {
         $path = $this->request->getPath();
         $method = $this->request->method();
-        
 
-        // trying to run a route from routes array 
+
+        // trying to run a route from routes array
         $callback = $this->routes[$method][$path] ?? false;
-        // if there is no such route added, we say it doesn't exist
-        if ($callback === false) : 
+
+        // if there is no such route added, we say not exist
+        if ($callback === false) :
             // 404
-            $this->response->setResponseCode(404); 
+            $this->response->setResponseCode(404);
             return $this->renderView('_404');
         endif;
 
         // if our callback value is string
         // $app->router->get('/about', 'about');
-        if(is_string($callback)) :
+        if (is_string($callback)) :
             return $this->renderView($callback);
         endif;
 
-        // if our callback is array we handle it with class instance
+        // if our callback is array we handle it whith class instance
         if (is_array($callback)) :
             $instance = new $callback[0];
             Application::$app->controller = $instance;
             $callback[0] = Application::$app->controller;
         endif;
 
-        // print "<pre>";
-        // var_dump($callback);
-        // var_dump($path);
-        // var_dump($method);
-        // print "</pre>";
-        // exit();
 
-        // page does exist, we call user function
+        // page dose exsist we call user function
         return call_user_func($callback, $this->request);
 
     }
@@ -105,57 +105,66 @@ class Router {
      * Renders the page and applies the layout
      *
      * @param string $view
-     * @return string / string []
+     * @return string|string[]
      */
-    public function renderView(string $view, array $params = []) {
+    public function renderView(string $view, array $params = [])
+    {
         $layout = $this->layoutContent();
         $page = $this->pageContent($view, $params);
-
-        // take layout and replace the content with the $page content
+//        echo $page;
+        // take layout and replace the {{content}} with the $page content
         return str_replace('{{content}}', $page, $layout);
 
-        // 
+        //
     }
 
     /**
      * Returns the layout HTML content
      *
-     * @return false / string
+     * @return false|string
      */
-    protected function layoutContent () {
-        $layout = Application::$app->controller->layout;
+    protected function layoutContent()
+    {
+        if (isset(Application::$app->controller)) :
+            $layout = Application::$app->controller->layout;
+        else :
+            $layout = 'main';
+        endif;
+
         // start buffering
         ob_start();
-        include_once Application::$ROOT_DIR."/view/layout/$layout.php";
+        include_once Application::$ROOT_DIR . "/view/layout/$layout.php";
         // stop and return buffering
         return ob_get_clean();
+
     }
 
     /**
      * Returns only the given page HTML content
      *
-     * @param [type] $view
-     * @return void
+     * @param $view
+     * @param $params
+     * @return false|string
      */
-    protected function pageContent($view, $params) {
-        // a smart way of creating variables dinamically        
+    protected function pageContent($view, $params)
+    {
+//        $params = [
+//            'name' => "AlmostLara",
+//            'subtitle' => "This is a nice way to learn PHP"
+//        ];
+        // a smart way of creating variables dinamically
         // $name = $params['name'];
-
-        foreach($params as $key => $value) :
-            $$key = $value;
+        foreach ($params as $key => $value) :
+            $$key = $value; // name = AlmostLara // $name = AlmostLara
         endforeach;
-
-
-        // print "<pre>";
-        // print_r($params);        
-        // print "</pre>";
-        // exit();
 
         // start buffering
         ob_start();
-        include_once Application::$ROOT_DIR."/view/$view.php";
+        include_once Application::$ROOT_DIR . "/view/$view.php";
         // stop and return buffering
         return ob_get_clean();
     }
+
+
 
 }
