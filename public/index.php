55eb01e4c6facac97df06ce4af70109b23708ae2
arnas13 <ses.arnas@gmail.com>
2021-02-25 09:01:10 +0200

    clean up index.php

--- public/index.php
+++ public/index.php
@@ -17,41 +17,35 @@ $config =[
     ]
 ];
 
-//echo "<pre>";
-//var_dump($config);
-//echo "</pre>";
-//exit;
-
 
 $app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/about', [SiteController::class, 'about']);
-//$app->router->get('/about', 'about');
 $app->router->get('/contact', [SiteController::class, 'contact']);
-$app->router->get('/fn', function () {
-    return 56 + 76;
-});
-// we create post path
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+// TODO: add to readme.md
+// we can run functions from any route
+// $app->router->get('/fn', function () {
+//     return 56 + 76;
+// });
+
 
 // routes for login and register
 $app->router->get('/login', [AuthController::class, 'login']);
 $app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/logout', [AuthController::class, 'logout']);
 
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
 
-$app->router->get('/logout', [AuthController::class, 'logout']);
 
-$app->router->get('/posts', [PostsController::class, 'index']);
 
-$app->router->get('/post/{id}', [PostsController::class, 'post']);
+// TODO: add to readme.md
+// $app->router->get('/post/{id}', [PostsController::class, 'post']);
 
-$app->router->get('/post/add', [PostsController::class, 'addPost']);
-$app->router->post('/post/add', [PostsController::class, 'addPost']);
-
-$app->router->get('/post/edit/{id}', [PostsController::class, 'editPost']);
+// $app->router->get('/post/edit/{id}', [PostsController::class, 'editPost']);
 
 
 $app->run();
